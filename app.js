d28e1f94efc7c0d2a1775455e0d96e99fa11622f
muborcik-cloud <muborcik@gmail.com>
2026-04-29 18:40:52 +0500

    Update app.js

--- app.js
+++ app.js
@@ -7,126 +7,138 @@ canvas.height = innerHeight;
 let money = 0;
 
 // игрок
-let player = {x:300, y:300, speed:3};
+let player = {
+  x:200,
+  y:200,
+  inCar:false,
+  car:null
+};
 
-// мир
+// объекты
+let objects = [];
 let cars = [];
-let npcs = [];
-let lights = [];
 
 // 💰 деньги
 setInterval(()=>{
-  money += 20;
+  money += 10;
   document.getElementById("money").innerText = money;
 },1000);
 
-// 🚗 машины
-for(let i=0;i<6;i++){
-  cars.push({
-    x: Math.random()*canvas.width,
-    y: Math.random()*canvas.height,
-    dir: Math.random()*2,
-    speed:2
-  });
-}
+// картинки
+const imgs = {
+ house:"https://cdn-icons-png.flaticon.com/512/69/69524.png",
+ shop:"https://cdn-icons-png.flaticon.com/512/34/34627.png",
+ hospital:"https://cdn-icons-png.flaticon.com/512/2967/2967350.png",
+ car:"https://cdn-icons-png.flaticon.com/512/743/743922.png"
+};
 
-// 👨 NPC
-for(let i=0;i<15;i++){
-  npcs.push({
-    x: Math.random()*canvas.width,
-    y: Math.random()*canvas.height
-  });
-}
+// покупка
+function buy(type){
+ if(money < 100) return;
+ money -= 100;
 
-// 🚦 светофоры
-for(let i=0;i<5;i++){
-  lights.push({
-    x: Math.random()*canvas.width,
-    y: Math.random()*canvas.height,
-    state: Math.random()>0.5 ? "red":"green"
-  });
-}
+ let obj = {
+   type,
+   x: player.x+60,
+   y: player.y
+ };
 
-// переключение светофора
-setInterval(()=>{
-  lights.forEach(l=>{
-    l.state = l.state==="red" ? "green":"red";
-  });
-},3000);
+ objects.push(obj);
+
+ if(type==="car") cars.push(obj);
+}
 
 // 🎮 джойстик
 let dx=0, dy=0;
 const stick = document.getElementById("stick");
 
 document.getElementById("joy").addEventListener("touchmove", e=>{
-  let t = e.touches[0];
-  dx = (t.clientX-80)/20;
-  dy = (t.clientY-(innerHeight-80))/20;
+ let t = e.touches[0];
 
-  stick.style.left = (20+dx*5)+"px";
-  stick.style.top = (20+dy*5)+"px";
+ dx = (t.clientX-80)/20;
+ dy = (t.clientY-(innerHeight-80))/20;
+
+ stick.style.left = (30+dx*10)+"px";
+ stick.style.top = (30+dy*10)+"px";
 });
 
 document.getElementById("joy").addEventListener("touchend", ()=>{
-  dx=dy=0;
-  stick.style.left="20px";
-  stick.style.top="20px";
+ dx=0; dy=0;
+ stick.style.left="30px";
+ stick.style.top="30px";
 });
 
-// 🎨 игра
-function draw(){
+// сесть в машину
+document.addEventListener("click", ()=>{
+ cars.forEach(c=>{
+   if(Math.abs(player.x-c.x)<40 && Math.abs(player.y-c.y)<40){
+     player.inCar = true;
+     player.car = c;
+   }
+ });
+});
 
-  ctx.fillStyle="#5fc45f";
-  ctx.fillRect(0,0,canvas.width,canvas.height);
-
-  // дороги
-  ctx.fillStyle="#888";
-  for(let i=0;i<canvas.width;i+=150){
-    ctx.fillRect(i,0,30,canvas.height);
-  }
-  for(let i=0;i<canvas.height;i+=150){
-    ctx.fillRect(0,i,canvas.width,30);
-  }
-
-  // 🚦 светофоры
-  lights.forEach(l=>{
-    ctx.fillStyle = l.state==="red" ? "red":"lime";
-    ctx.fillRect(l.x, l.y, 10,20);
-  });
-
-  // 🚗 машины
-  ctx.fillStyle="black";
-  cars.forEach(c=>{
-    c.x += Math.cos(c.dir)*c.speed;
-    c.y += Math.sin(c.dir)*c.speed;
-    ctx.fillRect(c.x, c.y, 30,15);
-  });
-
-  // 👨 NPC
-  ctx.fillStyle="yellow";
-  npcs.forEach(n=>{
-    n.x += Math.random()*2-1;
-    n.y += Math.random()*2-1;
-    ctx.fillRect(n.x,n.y,10,10);
-  });
-
-  // 🧍 игрок
-  ctx.fillStyle="blue";
-  ctx.fillRect(player.x,player.y,20,20);
-
-  ctx.strokeStyle="white";
-  ctx.beginPath();
-  ctx.moveTo(player.x+10,player.y+20);
-  ctx.lineTo(player.x,player.y+35);
-  ctx.moveTo(player.x+10,player.y+20);
-  ctx.lineTo(player.x+20,player.y+35);
-  ctx.stroke();
-
-  // движение
-  player.x += dx*player.speed;
-  player.y += dy*player.speed;
-
-  requestAnimationFrame(draw);
+// движение
+function update(){
+ if(player.inCar){
+   player.car.x += dx*5;
+   player.car.y += dy*5;
+   player.x = player.car.x;
+   player.y = player.car.y;
+ } else {
+   player.x += dx*3;
+   player.y += dy*3;
+ }
 }
 
-draw();
+// рисование
+function draw(){
+ ctx.clearRect(0,0,canvas.width,canvas.height);
+
+ // дороги
+ ctx.fillStyle="#888";
+ for(let i=0;i<canvas.width;i+=150){
+   ctx.fillRect(i,0,40,canvas.height);
+ }
+ for(let i=0;i<canvas.height;i+=150){
+   ctx.fillRect(0,i,canvas.width,40);
+ }
+
+ // объекты
+ objects.forEach(o=>{
+   let img = new Image();
+   img.src = imgs[o.type];
+   ctx.drawImage(img, o.x, o.y, 50,50);
+ });
+
+ // игрок (человечек)
+ if(!player.inCar){
+   ctx.fillStyle="blue";
+   ctx.fillRect(player.x,player.y,20,20);
+
+   ctx.fillStyle="peachpuff";
+   ctx.beginPath();
+   ctx.arc(player.x+10,player.y-5,6,0,Math.PI*2);
+   ctx.fill();
+
+   ctx.strokeStyle="black";
+   ctx.beginPath();
+   ctx.moveTo(player.x,player.y+10);
+   ctx.lineTo(player.x-10,player.y+20);
+   ctx.moveTo(player.x+20,player.y+10);
+   ctx.lineTo(player.x+30,player.y+20);
+   ctx.moveTo(player.x+5,player.y+20);
+   ctx.lineTo(player.x,player.y+30);
+   ctx.moveTo(player.x+15,player.y+20);
+   ctx.lineTo(player.x+20,player.y+30);
+   ctx.stroke();
+ }
+
+ requestAnimationFrame(loop);
+}
+
+function loop(){
+ update();
+ draw();
+}
+loop();
